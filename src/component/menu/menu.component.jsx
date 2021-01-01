187a86e57f562b188a048bce01e704ee3c5d87b5
sunildipun <sunil.6318ie@gmail.com>
2021-01-01 22:44:04 +0530

    menu details

--- src/component/menu/menu.component.jsx
+++ src/component/menu/menu.component.jsx
@@ -9,7 +9,15 @@ const Menu = ({items}) => {
                 items.map((menu, index) => {
                     return(
                         <article key={index}>
-                            <p>{menu.title}</p>
+
+                          <img src={menu.img} alt={menu.title} className="photo" />
+                            <div className="item-info">
+                            <header>
+                                <h4>{menu.title}</h4>
+                                <h4 className="price">{menu.price}</h4>
+                            </header>
+                            <p className="item-text">{menu.desc}</p>
+                            </div>
                         </article>
                     )
                     }
